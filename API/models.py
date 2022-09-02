ce1756f895ec0aaddcd52dd244a201c0a45b7a63
mausam-office <eng3@ramlaxmangroup.com>
2022-09-02 10:11:08 +0545

    Modified column `audio_base64` to be null in database

--- API/models.py
+++ API/models.py
@@ -3,6 +3,11 @@ from django.db import models
 
 class Audio(models.Model):
     device_name = models.CharField(max_length=150)
-    audio_base64 = models.CharField(max_length=130000)
+    audio_base64 = models.CharField(max_length=130000, null=True)
     is_sent = models.BooleanField(default=False)
     audio_base64_text = models.TextField(default='')
+    # TODO add a field to for a
+
+# class DeviceApproval(models.Model):
+#     device_name = models.CharField(max_length=150)
+#     approval_status = models.BooleanField(default=False)
