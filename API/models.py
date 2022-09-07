bfad4c63a6b6acb99dbc1654dc85e285ef88e8be
mausam-office <eng3@ramlaxmangroup.com>
2022-09-07 11:29:53 +0545

    git reset

--- API/models.py
+++ API/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Audio(models.Model):
     device_name = models.CharField(max_length=150)
-    audio_base64 = models.CharField(max_length=130000, null=True)
+    audio_base64 = models.CharField(max_length=130000, null=True) # should remove this in future.
     is_sent = models.BooleanField(default=False)
     audio_base64_text = models.TextField(default='')
     # added a field to determine inactive devices.
